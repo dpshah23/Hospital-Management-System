b20f2bb9594dadba1b74f9969c1fbd235b046341
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-04-16 18:30:29 +0530

    Added Show Appointments

--- main.java
+++ main.java
@@ -1,15 +1,74 @@
-import java.sql.*;
+import java.util.*;
+import java.io.*;
 
-class hospital{
-    public static void main(String[] args) {
-        String url="postgres://deep:wgXBHnl1CkbwAwpzQ9Av9COAVbQqcf9l@dpg-codljj8l5elc73fqb91g-a.singapore-postgres.render.com/hospital_management_system_4htd";
-        String username="deep";
-        String password="wgXBHnl1CkbwAwpzQ9Av9COAVbQqcf9l";
-        try{
-        Connection db=DriverManager.getConnection(url, username, password);
+class patients{
+    void showpatients(){
+        String file="patients.csv";
+        BufferedReader reader=null;
+        String line="";
+        try {
+            reader=new BufferedReader(new FileReader(file));
+            while((line=reader.readLine()) !=null){
+                String[] row=line.split(",");
+                for (String index : row) {
+                    System.out.printf("%-10s",index);
+                }
+                System.out.println();
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error Occured...");
         }
-        catch(Exception e){
-            System.out.println("Error Occured "+e);
+       finally{
+        try {
+            reader.close();
+        } catch (IOException e) {
+           
+            e.printStackTrace();
+        }
+       }
+    }
+}
+class hospitalmanagement{
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        while(true){
+        try{
+        System.out.println("1. Add New Patient Details");
+        System.out.println("2. Update Patient Details");
+        System.out.println("3. Show All Patients Details");
+        System.out.println("4. Create New Appointment");
+        System.out.println("5. See All Appointments");
+        System.out.println("6. Search Patient");
+        System.out.print("Enter Your choice: ");
+        int no=sc.nextInt();
+
+        switch (no) {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                patients showtotal=new patients();
+                showtotal.showpatients();
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        
+            default:
+                break;
         }
     }
+    catch(Exception e){
+        System.out.println("Error Occured....");
+    }
+    }
+}
 }
