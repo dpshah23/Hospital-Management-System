6c16955815925b3cab83f7be822b66cbf186d223
VedantTuvar <vedanttuvar01@gmail.com>
2024-04-17 09:57:02 +0530

    Updated Code

--- main.java
+++ main.java
@@ -8,29 +8,29 @@ class patients {
         String line = "";
         try {
             reader = new BufferedReader(new FileReader(file));
-            while((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
                 for (String index : row) {
-                    System.out.printf("%-10s",index);
+                    System.out.printf("%-10s", index);
                 }
                 System.out.println();
             }
-            
+
         } catch (Exception e) {
             System.out.println("Error Occured...");
         }
 
-       finally {
-        try {
-            reader.close();
-        } 
-        
-        catch (IOException e) {
-           
-            e.printStackTrace();
-        }
+        finally {
+            try {
+                reader.close();
+            }
 
-       }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
@@ -50,18 +50,16 @@ class patients {
             sc.nextLine(); // Consume newline character
             System.out.print("Enter Patient Mobile Number: ");
             String mobile = sc.nextLine();
-            
+
             String str = String.format("%d,%s,%d,%s%n", id, name, age, mobile);
             writer.append(str);
-    
+
             System.out.println("Data Added Successfully !");
             writer.close(); // Close the FileWriter
-            
+
         } catch (Exception e) {
             System.out.println("Some Error Occurred: " + e);
-        }
-        finally
-        {
+        } finally {
             sc.close();
         }
     }
@@ -79,9 +77,9 @@ class patients {
             reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
-                if (row.length >= 2 && row[1].equalsIgnoreCase(name)) { 
-                        for (String index : row) {
-                            System.out.printf("%-10s", index);
+                if (row.length >= 2 && row[1].equalsIgnoreCase(name)) {
+                    for (String index : row) {
+                        System.out.printf("%-10s", index);
                     }
                     System.out.println();
                 }
@@ -99,16 +97,15 @@ class patients {
             }
         }
     }
-    
-    
+
 }
 
-class appointments{
-    void addappointment(){
+class appointments {
+    void addappointment() {
         Scanner sc = new Scanner(System.in);
         try {
             String file = "appointments.csv";
-            FileWriter writer = new FileWriter(file, true); 
+            FileWriter writer = new FileWriter(file, true);
             System.out.print("Enter Patient Id: ");
             String id = sc.nextLine();
             sc.nextLine(); // Consume newline character
@@ -116,20 +113,19 @@ class appointments{
             String name = sc.nextLine();
             System.out.print("Enter Patient Age: ");
             int age = sc.nextInt();
-            sc.nextLine(); 
+            sc.nextLine();
             System.out.print("Enter Appointment Type: ");
             String type = sc.nextLine();
-    
+
             String str = String.format("%s,%s,%d,%s%n", id, name, age, type);
             writer.append(str);
-    
+
             System.out.println("Appointment Created Successfully !");
-            writer.close(); 
-            
+            writer.close();
+
         } catch (Exception e) {
             System.out.println("Some Error Occurred: " + e);
-        }
-        finally{
+        } finally {
             sc.close();
         }
 
@@ -142,62 +138,58 @@ class appointments{
         String line = "";
         try {
             reader = new BufferedReader(new FileReader(file));
-            while((line = reader.readLine()) !=null){
+            while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
                 for (String index : row) {
-                    System.out.printf("%-10s",index);
+                    System.out.printf("%-10s", index);
                 }
                 System.out.println();
             }
-            
+
         } catch (Exception e) {
             System.out.println("Error Occured...!");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
         }
-       finally{
-        try {
-            reader.close();
-        } catch (IOException e) {
-           
-            e.printStackTrace();
-        }
-       }
 
     }
 }
 
-class searchappointments{
-    void redirect()
-    {   
+class searchappointments {
+    void redirect() {
         Scanner sc = new Scanner(System.in);
-        try{
-        System.out.println("Search Appointments");
-        System.out.println("1. Search By Name ");
-        System.out.println("2. Search By ID");
-        System.out.println("3. Search By Appointment Type");
-        System.out.print("Enter Choice: ");
-        int choice = sc.nextInt();
+        try {
+            System.out.println("Search Appointments");
+            System.out.println("1. Search By Name ");
+            System.out.println("2. Search By ID");
+            System.out.println("3. Search By Appointment Type");
+            System.out.print("Enter Choice: ");
+            int choice = sc.nextInt();
 
-        switch (choice) {
-            case 1:
-                this.searchbyname();
-                break;
-            case 2:
-                this.searchbyid();
-                break;
-            case 3:
-                this.searchbytype();
-                break;
-            default:
-                System.out.println("Invalid Choice...!");
-                break;
+            switch (choice) {
+                case 1:
+                    this.searchbyname();
+                    break;
+                case 2:
+                    this.searchbyid();
+                    break;
+                case 3:
+                    this.searchbytype();
+                    break;
+                default:
+                    System.out.println("Invalid Choice...!");
+                    break;
+            }
+        } catch (Exception e) {
+
+        } finally {
+            sc.close();
         }
-    }
-    catch(Exception e) {
-
-    }
-    finally {
-        sc.close();
-    }
 
     }
 
@@ -214,9 +206,9 @@ class searchappointments{
             reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
-                if (row.length >= 2 && row[1].equalsIgnoreCase(name)) { 
-                        for (String index : row) {
-                            System.out.printf("%-10s", index);
+                if (row.length >= 2 && row[1].equalsIgnoreCase(name)) {
+                    for (String index : row) {
+                        System.out.printf("%-10s", index);
                     }
                     System.out.println();
                 }
@@ -233,10 +225,10 @@ class searchappointments{
                 e.printStackTrace();
             }
         }
-    
+
     }
-    void searchbyid()
-    {
+
+    void searchbyid() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter ID To Search : ");
         String no = sc.nextLine();
@@ -249,10 +241,10 @@ class searchappointments{
             reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
-               
-                if (row.length >= 2 && row[0].equals(no)) { 
-                        for (String index : row) {
-                            System.out.printf("%-10s", index);
+
+                if (row.length >= 2 && row[0].equals(no)) {
+                    for (String index : row) {
+                        System.out.printf("%-10s", index);
                     }
                     System.out.println();
                 }
@@ -269,11 +261,10 @@ class searchappointments{
                 e.printStackTrace();
             }
         }
-    
+
     }
 
-    void searchbytype()
-    {
+    void searchbytype() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Appointment Type To Search: ");
         String type = sc.nextLine();
@@ -286,10 +277,10 @@ class searchappointments{
             reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
-               
-                if (row.length >= 2 && row[3].equalsIgnoreCase(type)) { 
-                        for (String index : row) {
-                            System.out.printf("%-10s", index);
+
+                if (row.length >= 2 && row[3].equalsIgnoreCase(type)) {
+                    for (String index : row) {
+                        System.out.printf("%-10s", index);
                     }
                     System.out.println();
                 }
@@ -309,68 +300,66 @@ class searchappointments{
 
     }
 
-    }
+}
 
-
-class hospitalmanagement{
+class hospitalmanagement {
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        while(true){
-        try{
-        System.out.println("<------- Welcome to Hospital Management System ------->");
-        System.out.println();
-        System.out.println("1. Add New Patient Details");
-        System.out.println("2. Show All Patients Details");
-        System.out.println("3. Create New Appointment");
-        System.out.println("4. See All Appointments");
-        System.out.println("5. Search Patient");
-        System.out.println("6. Search Appointments");
-        System.out.println("7. Exit");
-        System.out.print("Enter Your choice: ");
-        System.out.println();
-        int no=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("<------- Welcome to Hospital Management System ------->");
+                System.out.println();
+                System.out.println("1. Add New Patient Details");
+                System.out.println("2. Show All Patients Details");
+                System.out.println("3. Create New Appointment");
+                System.out.println("4. See All Appointments");
+                System.out.println("5. Search Patient");
+                System.out.println("6. Search Appointments");
+                System.out.println("7. Exit");
+                System.out.print("Enter Your choice: ");
+                System.out.println();
+                int no = sc.nextInt();
 
-        switch (no) {
-            case 1:
-                patients addpatients = new patients();
-                addpatients.addpatient();
-                break;
-            case 2:
-                patients showtotal = new patients();
-                showtotal.showpatients();
-                break;
-            case 3:
-                appointments addAppointments = new appointments();
-                addAppointments.addappointment();
-                break;
-            case 4:
-                appointments seeall = new appointments();
-                seeall.seeallappointments();
-                break;
-            case 5:
-                patients seach = new patients();
-                seach.search();
-                break;
-            case 6:
-                searchappointments sa = new searchappointments();
-                sa.redirect();
-                break;
-            case 7:
-                sc.close();
-                System.exit(1);
-            default:
-                System.out.println("Invalid Choice...");
-                break;
+                switch (no) {
+                    case 1:
+                        patients addpatients = new patients();
+                        addpatients.addpatient();
+                        break;
+                    case 2:
+                        patients showtotal = new patients();
+                        showtotal.showpatients();
+                        break;
+                    case 3:
+                        appointments addAppointments = new appointments();
+                        addAppointments.addappointment();
+                        break;
+                    case 4:
+                        appointments seeall = new appointments();
+                        seeall.seeallappointments();
+                        break;
+                    case 5:
+                        patients seach = new patients();
+                        seach.search();
+                        break;
+                    case 6:
+                        searchappointments sa = new searchappointments();
+                        sa.redirect();
+                        break;
+                    case 7:
+                        sc.close();
+                        System.exit(1);
+                    default:
+                        System.out.println("Invalid Choice...");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error Occured....!" + e);
+            }
+
         }
+
     }
-    catch(Exception e){
-        System.out.println("Error Occured....!"+e);
-    }
-    
-    }
-     
-}
 
 }
